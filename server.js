ed466565c46999171834e7bb4afdd5eb7eea8067
cmwschroeder <cmwschroeder@gmail.com>
2022-05-06 09:53:25 -0700

    added comments to server.js, created connection in connection.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+//inpomrt node packages
 const path = require('path');
 const express = require('express');
-// Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
+//get links to routes and connection
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 

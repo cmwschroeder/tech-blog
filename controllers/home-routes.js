21bc452f37bf868c955bd4e8387870ffd7f326f4
cmwschroeder <cmwschroeder@gmail.com>
2022-05-07 11:16:33 -0700

    made is so that home route opens up very basic html page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
-    
+router.get('/', (req, res) => {
+    try {
+        res.render('homepage');
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 //get links to routes and connection
 const routes = require('./controllers');

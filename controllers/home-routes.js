3f4b37eeb155bbc20031a2ad7b2b41217b53fbc0
cmwschroeder <cmwschroeder@gmail.com>
2022-05-08 14:08:17 -0700

    fixed navbar links besides dashboard when logged in, display posts on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,33 @@
 const router = require('express').Router();
+const { User, Post } = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
+        const postData = await Post.findAll({
+            include: [{ model: User }]
+        }).catch((err) => { 
+            res.json(err);
+        });
+
+        const userPosts = postData.map((post) => post.get({ plain: true}));
+
+        const posts = userPosts.map((post) => {
+            return {
+                id: post.id,
+                title: post.title,
+                content: post.content,
+                date_created: post.date_created,
+                user_id: post.user_id,
+                user: {
+                    user_id: post.user.id,
+                    name: post.user.name,
+                },
+            }
+        });
+        
         res.render('homepage', {
         loggedIn: req.session.loggedIn,
+        posts: posts,
     });
     } catch (err) {
         res.status(500).json(err);
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,21 +2,25 @@ const { Post } = require('../models');
 
 const postData = [
     {
+        title: "First post",
         content: "This is my super cool first post",
         date_created: "8/7/21",
         user_id: 1,
     },
     {
+        title: "Thoughts on web dev",
         content: "Web development is super fun",
         date_created: "11/13/21",
         user_id: 2,
     },
     {
+        title: "Thoughts on blogs",
         content: "Blogs are pretty cool",
         date_created: "12/12/21",
         user_id: 3,
     },
     {
+        title: "How we are",
         content: "We are having a great time",
         date_created: "2/3/22",
         user_id: 1,
